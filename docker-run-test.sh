890f642862fac4dd879f9dc57b576b5eb3f7dba5
scjtqs <scjtqs@qq.com>
2021-08-02 11:07:54 +0800

    fix

--- docker-run-test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-# docker network create --subnet=172.18.0.0/16 mynetwork
-docker run --rm -it --network host -e PublicIp=172.18.0.10 -v "`pwd`/mysql/":/home/data/  scjtqs/dnf-centos5:latest
